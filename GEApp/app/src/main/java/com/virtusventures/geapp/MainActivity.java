47e18f79ca5741d5a2539b8317e48f3513dff57e
codeatena <codeatena@yahoo.com>
2017-03-15 16:43:14 +0800

    add project

---
+++ GEApp/app/src/main/java/com/virtusventures/geapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.virtusventures.geapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
